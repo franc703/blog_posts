f3f45648d5681fe4435a8fe311459486dd89e3f7
Rodrigo Franco <franc703@umn.edu>
2022-08-08 13:43:55 -0500

    Add comments to clarify all the steps

--- blog_post_1_kfold/code/k_fold_cross_validation.R
+++ blog_post_1_kfold/code/k_fold_cross_validation.R
@@ -10,15 +10,17 @@ n <- nrow(Hitters)
 
 # Create the folds: we sample from (1,2,3,..., k) as many times as we have rows
 
+# Create the folds, we will use this in the loop to extract the folds for training and testing
 folds <- sample(rep(1:k, length = n))
+# Create a matrix where we will deposit the test MSE
 cv.kfold <- matrix(NA, k, 1)
 
 # Generate the MSE (Mean Squared Errors) for the test groups
 for (j in 1:k) {
-  lm.fit <- lm(Salary ~ ., data = Hitters[folds != j, ])
-  pred <- predict(lm.fit, Hitters[folds == j, ])
-  cv.kfold[j] <- mean((Hitters$Salary[folds == j] - pred)^2)
+  lm.fit <- lm(Salary ~ ., data = Hitters[folds != j, ]) #    Run the model for the training set of folds
+  pred <- predict(lm.fit, Hitters[folds == j, ]) #    Get prediction for the test set
+  cv.kfold[j] <- mean((Hitters$Salary[folds == j] - pred)^2) #    Calculate the MSE and deposit it in the vector
 }
 
 # Average the errors to have the k-fold CV MSE
-mean.kfold.error <- apply(cv.kfold, 2, mean)
+mean.kfold.error <- apply(cv.kfold, 2, mean) #    Average the k MSE
