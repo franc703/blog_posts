22f796e891a5db7d829808dff75c9bbbd02d69a3
Rodrigo Franco <franc703@umn.edu>
2022-08-08 13:38:51 -0500

    Add the for loop to creat the cross-validation errors

---
+++ blog_post_1_kfold/code/k_fold_cross_validation.R
@@ -0,0 +1,21 @@
+# Load packages
+library(ISLR2)
+
+# Drop the missing values
+Hitters <- na.omit(Hitters)
+
+# Set up the parameters we need
+k <- 10
+n <- nrow(Hitters)
+
+# Create the folds: we sample from (1,2,3,..., k) as many times as we have rows
+
+folds <- sample(rep(1:k, length = n))
+cv.kfold <- matrix(NA, k, 1)
+
+# Generate the MSE (Mean Squared Errors) for the test groups
+for (j in 1:k) {
+  lm.fit <- lm(Salary ~ ., data = Hitters[folds != j, ])
+  pred <- predict(lm.fit, Hitters[folds == j, ])
+  cv.kfold[j] <- mean((Hitters$Salary[folds == j] - pred)^2)
+}
